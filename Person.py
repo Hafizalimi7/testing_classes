ca3568f1b385894bbd2554cdfee598a232bd1e9e
Hafiz <hafizalimi77@gmail.com>
2023-12-20 18:29:05 +0000

    few passing tests

--- Person.py
+++ Person.py
@@ -1,13 +1,30 @@
 class Unknown:
 
   def __init__(self, f_name, l_name, age) -> None:
+
+    # def raise_error_(self):
+    #   if not isinstance(self.f_name, str):
+    #     raise TypeError("First name should be a string")
+    #   if not isinstance(self.l_name, str):
+    #     raise TypeError("Last name should be a string")
+    #   if not isinstance(self.age, int):
+    #     raise TypeError("Age has to be a interger")
+
+
     self.f_name = f_name
     self.l_name = l_name 
     self.age = age
     self.wage = 50
     self.year_salary = 0
     self.hobbies = []
-  
+    
+  def raise_error(self):
+      if not isinstance(self.f_name, str):
+        raise TypeError("First name should be a string")
+      if not isinstance(self.l_name, str):
+        raise TypeError("Last name should be a string")
+      if not isinstance(self.age, int):
+        raise TypeError("Age has to be a interger")
 
   def first_name(self) -> str:
     return f"{self.f_name}"
@@ -16,7 +33,7 @@ class Unknown:
     return f"{self.l_name}"
   
   def full_name(self) -> str:
-    return f"{self.f_name + " " + self.l_name}"
+    return f"{self.f_name + ' ' + self.l_name}"
   
   def person_age(self) -> int:
     return f"{self.age}"
